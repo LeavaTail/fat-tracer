5a931f92b1489235cc7d6d94afcbc2f12ce4b7aa
LeavaTail <starbow.duster@gmail.com>
2019-10-19 21:44:25 +0900

    [Fix] #6 Not test all option.

--- tests/usage.sh
+++ tests/usage.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
 ./fatracer --help
+if [ $? -gt 0 ]; then
+  exit 1;
+fi
+
 ./fatracer --version
+if [ $? -gt 0 ]; then
+  exit 1;
+fi
+
+./fatracer --none
+if [ $? -eq 0 ]; then
+  exit 1;
+fi
+
+exit 0;
